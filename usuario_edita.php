58a66f388b2f3e1c50876d70c91d6d4210f3b05d
igor <mouradeoliveiraigor10@gmail.com>
2025-05-26 21:24:16 -0300

    aula 26/05

--- usuario_edita.php
+++ usuario_edita.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>PDO com PHP - Usuários</title>
+    <title>PDO com PHP - Editar Usuário</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
 
@@ -25,10 +25,11 @@
 
 include_once('conexao.php');
 
-
-sql = "SELECT * FROM usuario_igor WHERE status LIKE 1";
+$sql = "SELECT * FROM usuario_neto WHERE status LIKE 1";
 
 $executa = $conn->prepare($sql);
+
+$executa->execute();
 
 $resultados = $executa->fetchAll(PDO::FETCH_OBJ);
 
@@ -44,8 +45,8 @@ $resultados = $executa->fetchAll(PDO::FETCH_OBJ);
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
-                        <a class="nav-item nav-link active" href="http://192.168.1.35/TRILHAS4/igormoura/pdo_com_php/">Home<span class="sr-only">(Página atual)</span></a>
-                        <a class="nav-item nav-link" href="http://192.168.1.35/TRILHAS4/igormoura/pdo_com_php/usuarios.php">Usuários</a>
+                        <a class="nav-item nav-link active" href="http://192.168.1.25/TRILHAS4/igormoura/pdo_com_php/">Home<span class="sr-only">(Página atual)</span></a>
+                        <a class="nav-item nav-link" href="http://192.168.1.25/TRILHAS4/igormoura/pdo_com_php/usuarios.php">Usuários</a>
                         <a class="nav-item nav-link disabled" href="#">Preços</a>
                         <a class="nav-item nav-link disabled" href="#">Desativado</a>
                     </div>
@@ -55,11 +56,111 @@ $resultados = $executa->fetchAll(PDO::FETCH_OBJ);
 
         <main id="principal">
 
-            <h4 class="title-prinpipal text-center mt-4">Lista de Usuários</h4>
+            <h4 class="title-prinpipal text-center mt-4">Editar Usuário</h4>
 
             <div class="jumbotron">
 
-                
+            <?php
+
+            include_once('conexao.php');
+
+            $id = $_GET['id'];
+
+            $querySQL = "SELECT * FROM usuario_igor WHERE cod_usuario LIKE $id";
+
+            $executa = $conn->prepare($querySQL);
+
+            $executa->execute();
+
+            $usuario = $executa->fetch(PDO::FETCH_OBJ);
+
+            // echo '<pre>';
+
+            // var_dump($usuario);
+
+            // echo '</pre>';
+
+            ?>
+
+                <form action="" method="POST">
+
+                    <div class="row form-group">
+                        <div class="col-3">
+                            <label for="txt-nome">Nome:</label>
+                        </div>
+                        <div class="col-9">
+                            <input type="text" name="txt-nome" id="txt-nome" class="form-control" value="<?= $usuario->nome; ?>" >
+                        </div>
+                    </div>
+
+                    <div class="row form-group">
+                        <div class="col-3">
+                            <label for="txt-email">E-Mail:</label>
+                        </div>
+                        <div class="col-9">
+                            <input type="email" name="txt-email" id="txt-email" class="form-control" value="<?= $usuario->email; ?>" >
+                        </div>
+                    </div>
+
+                    <div class="row form-group">
+                        <div class="col-3">
+                            <label for="txt-senha">Senha:</label>
+                        </div>
+                        <div class="col-3">
+                            <input type="password" name="txt-senha" id="txt-senha" class="form-control" >
+                        </div>
+                    </div>
+
+                    <div class="row form-group">
+                        <div class="col-3">
+                            <label for="txt-con-senha">Confirma Senha:</label>
+                        </div>
+                        <div class="col-3">
+                            <input type="password" name="txt-con-senha" id="txt-con-senha" class="form-control" >
+                        </div>
+                    </div>
+
+                    <div class="row form-group">
+                        <a href="javascript: void()" onclick="history.back()" class="btn btn-danger col-3 m-2">Cancelar</a>
+                        <input type="submit" name="bt-salvar" id="bt-salvar" class="btn btn-success col-3 m-2" value="Salvar" disabled/>
+                    </div>
+
+                </form>
+
+                <script>
+
+                   $$(document).ready(function() {
+
+                    $('input').change(function() {
+
+                        $('#bt-salvar').attr('disabled', false);
+
+                    });
+
+                    $('#txt-con-senha').change(function() {
+
+                        if( $(this).val() != $('#txt-senha').val() || $(this).val() == '' || $('#txt-senha').val()== '' ) {
+
+                            $(this).addClass('is-invalid');
+                            $('#txt-senha').addClass('is-invalid');
+                            $('#bt-salvar').attr('disabled', true);
+
+                        } else {
+
+                            $(this).addClass('is-valid');
+                            $(this).removeClass('is-invalid');
+                            $('#txt-senha').addClass('is-valid');
+                            $('#txt-senha').removeClass('is-invalid');
+                            $('#bt-salvar').attr('disabled', false);
+
+                        }
+
+                    });
+
+                });
+
+                </script>
+
             </div>
 
         </main>
